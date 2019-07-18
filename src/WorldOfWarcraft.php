852e0c496b36f764aa87e8d6717f9db6b4f09961
Brian Lamb <fusani.applications@gmail.com>
2019-07-18 00:28:53 +0000

    [T1195] Add method for getting realms

--- src/WorldOfWarcraft.php
+++ src/WorldOfWarcraft.php
@@ -86,6 +86,42 @@ class WorldOfWarcraft
     }
 
     /**
+     * Get all the realms for the specified region
+     *
+     * @param string $region
+     * @return array
+     */
+    public function realms(string $region) : array
+    {
+        $params = [
+            'access_token' => $this->accessToken,
+            'namespace' => 'dynamic-'.strtolower($region),
+        ];
+
+        // temporarily switch regions if needed
+        $baseUrl = $this->apiAdapter->getBaseUrl();
+        $this->apiAdapter->changeBaseUrl(str_replace('us.', "$region.", $baseUrl));
+
+        $response = $this->apiAdapter->get("/data/wow/realm/index", $params);
+
+        $this->apiAdapter->changeBaseUrl($baseUrl);
+
+        if (!empty($response['statusCode']) && $response['statusCode'] == 401) {
+            $this->refreshToken();
+
+            $params = [
+                'access_token' => $this->accessToken,
+                'fields' => 'reputation',
+            ];
+
+            $response = $this->apiAdapter->get("/data/wow/realm/index", $params);
+            $response['access_token'] = $params['access_token'];
+        }
+
+        return $response;
+    }
+
+    /**
      * Update the access token for api requests.
      *
      * @return void
