8d8f066beb4e24c7792990676d3a76d6a6229d19
Brian Lamb <fusani.applications@gmail.com>
2019-08-02 01:17:31 +0000

    [T1150] Add method for getting mounts

--- src/WorldOfWarcraft.php
+++ src/WorldOfWarcraft.php
@@ -58,6 +58,44 @@ class WorldOfWarcraft
     }
 
     /**
+     * Get mounts collected for the given character
+     *
+     * @param string $character
+     * @param string $realm
+     * @param string $region
+     * @return array
+     */
+    public function getMountsForCharacter(
+        string $character,
+        string $realm,
+        string $region
+    ) : array {
+        $params = [
+            'access_token' => $this->accessToken,
+            'fields' => 'mounts',
+        ];
+
+        // temporarily switch regions if needed
+        $baseUrl = $this->apiAdapter->getBaseUrl();
+        $this->apiAdapter->changeBaseUrl(str_replace('us.', "$region.", $baseUrl));
+
+        $response = $this->apiAdapter->get("/wow/character/$realm/$character", $params);
+
+        $this->apiAdapter->changeBaseUrl($baseUrl);
+
+        if (!empty($response['statusCode']) && $response['statusCode'] == 401) {
+            $this->refreshToken();
+
+            $params['access_token'] = $this->accessToken;
+
+            $response = $this->apiAdapter->get("/wow/character/$realm/$character", $params);
+            $response['access_token'] = $params['access_token'];
+        }
+
+        return $response;
+    }
+
+    /**
      * Get the item details based on the item id
      *
      * @param int $id : the wow api item id
