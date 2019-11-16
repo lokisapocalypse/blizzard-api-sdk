51cbd9310593b15338196ce9b23f24b0ccd8457c
Brian Lamb <fusani.applications@gmail.com>
2019-11-16 01:47:50 +0000

    [T1249] Add a faction service for getting faction data

--- src/WorldOfWarcraft/Reputation.php
+++ src/WorldOfWarcraft/Reputation.php
@@ -8,36 +8,6 @@ use Guzzle\Http\Exception;
 class Reputation extends WorldOfWarcraft
 {
     /**
-     * Get all the factions
-     *
-     * @param string $region
-     * @return array
-     */
-    public function allFactions(string $region = 'us') : array
-    {
-        $params = [
-            'access_token' => $this->accessToken,
-            'namespace' => 'static-' . $region,
-            'region' => $region,
-        ];
-
-        $response = $this->apiAdapter->get("/data/wow/reputation-faction/index", $params);
-
-        // if the token has expired, refresh and try again but only once
-        if (!empty($response['statusCode']) && $response['statusCode'] == 401) {
-            $this->refreshToken();
-
-            $params['access_token'] = $this->accessToken;
-
-            // we also need to update the access token as part of the response
-            $response = $this->apiAdapter->get("/data/wow/reputation-faction/index", $params);
-            $response['access_token'] = $params['access_token'];
-        }
-
-        return $response;
-    }
-
-    /**
      * Get all reputation tiers
      *
      * @param string $region
